a912a712f06f13a1cb00be4720f1eef3af593e21
EngJao89 <eng_joao@outlook.com>
2023-02-08 16:28:38 -0300

    feat(01-home): implementa home com logo do app

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 173,
+    paddingLeft: 154,
+    backgroundColor: '#0D0D0D',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  tasks: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  tasksCounter: {
+    fontSize: 15,
+    color: '#FFF',
+    fontFamily: 'Inter-Regular',
+  },
+  tasksCounterBold: {
+    fontSize: 15,
+    color: '#FFF',
+    fontFamily: 'Inter-Bold',
+  }
+});
