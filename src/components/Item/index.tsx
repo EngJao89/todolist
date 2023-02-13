a8fc63755041e795e24d902ce0f21a090dc80951
EngJao89 <eng_joao@outlook.com>
2023-02-13 20:49:44 -0300

    feat(07-item): implementa componente de item com lista dinamica

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Image, TouchableOpacity, Text, StyleSheet, TextInput} from 'react-native';
+
+import { styles } from './styles';
+
+import trashIcon from '../assets/icons/trash/trash.png';
+import editIcon from '../assets/icons/edit/edit.png';
+
+import Icon from 'react-native-vector-icons/Feather';
+import { List } from '../List';
+import { EditTaskArgs } from '../pages/Home';
+
+
+interface ItemProps {
+  task: Task;
+  toggleTaskDone: (id: number) => void;
+  removeTask: (id: number) => void;
+  editTask: ({taskId, taskNewTitle}: EditTaskArgs) => void;
+}
+
+export default function Item({ task, toggleTaskDone, removeTask, editTask }: ItemProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [taskTitleValue, setTaskTitleValue] = useState(task.title);
+  const textInputRef = useRef<TextInput>(null);
+
+  function handleStartEditing() {
+    setIsEditing(true);
+  }
+
+  function handleFinishEditing() {
+    setTaskTitleValue(task.title);
+    setIsEditing(false);
+  }
+
+  function handleSubmitEditing() {
+    editTask({ taskId: task.id, taskNewTitle: taskTitleValue });
+    setIsEditing(false);
+  }
+
+  useEffect(() => {
+    if (textInputRef.current) {
+      if (isEditing) {
+        textInputRef.current.focus();
+      } else {
+        textInputRef.current.blur();
+      }
+    }
+  } , [isEditing]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.infoContainer}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.taskButton}
+          onPress={() => toggleTaskDone(task.id)}
+        >
+        <View 
+          style={task.done ? styles.taskMarkerDone : styles.taskMarker}
+        >
+          { task.done && (
+            <Icon 
+              name="check"
+              size={12}
+              color="#FFF"
+            />
+          )}
+      </View>
+
+        <TextInput 
+          value={taskTitleValue}
+          onChangeText={setTaskTitleValue}
+          editable={isEditing}
+          onSubmitEditing={handleSubmitEditing}
+          style={task.done ? styles.taskTextDone : styles.taskText}
+        />
+        </TouchableOpacity>
+    </View>
+
+      <View style={styles.iconsContainer}>
+        { isEditing ?(
+          <TouchableOpacity
+            onPress={handleFinishEditing}
+          >
+            <Icon 
+              name="x"
+              size={24}
+              color="#b2b2b2"
+            />
+          </TouchableOpacity>
+        ) :(
+          <TouchableOpacity
+            onPress={handleStartEditing}
+          >
+            <Image source={editIcon} />
+          </TouchableOpacity>
+        )}
+      </View>
+
+      <View style={styles.iconsDivider} />
+
+      <TouchableOpacity
+        onPress={() => removeTask(task.id)}
+        disabled={isEditing}
+      >
+        <Image source={trashIcon} style={{ opacity: isEditing ? 0.2 : 1 }}/>
+      </TouchableOpacity>
+    </View>
+  );
+}
